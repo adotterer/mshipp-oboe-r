1744e5a1f43be74b2d4690c58ae46347ff83252c
Andrew Dotterer <andrew.dotterer@overdose.digital>
2024-05-02 18:16:14 -0400

    New bio

--- components/Bio.js
+++ components/Bio.js
@@ -4,6 +4,19 @@ export default function Bio() {
       <article id="article__bio">
         <h2>Oboe & English Horn</h2>
         <p>
+        Matthew Shipp enjoys a rich and varied career as a sought-after performer and teacher. Matthew performs frequently as a guest musician with The Philadelphia Orchestra, among other distinguished ensembles in the region, including the Reading Symphony Orchestra, Chamber Orchestra of Philadelphia, Princeton Symphony, Black Pearl Chamber Orchestra, Bay-Atlantic Symphony, and Symphony in C.
+      </p>
+      <p>
+As a guest musician at The Philadelphia Orchestra, Mx. Shipp has performed both domestically and internationally in Taiwan, Japan, and South Korea. Notable engagements include performances at Carnegie Hall, Suntory Hall, NHK Hall, Seoul Arts Center, and the National Performing Arts Center of Taiwan. 
+</p>
+Mx. Shipp has been fortunate to collaborate with some of the world&#39;s leading conductors, including Yannick Nézet-Séguin, Esa-Pekka Salonen, Paavo Järvi, Jeri Lynn Johnson, Rafael Payare, William Eddins, and Christoph Eschenbach. Furthermore, Matthew has performed alongside some of the world&#39;s most illustrious soloists, including Joshua Bell, Hilary Hahn, Seong-Jin Cho, Hélène Grimaud, Sir András Schiff, Haochen Zhang, and Inon Barnatan. 
+<p>
+As a chamber musician, Matthew performed the Beethoven Wind Octet Op.103 with members of The Philadelphia Orchestra as a co-presenter at the CBDNA 2020 Conference. Additionally, Matthew was a featured soloist in Mozart&#39;s aria “Deh vieni, non tardar” with the Temple Chamber Winds at the World Association for Symphonic Bands and Ensembles (WASBE) International Conference. Matthew has spent summers participating in music festivals such as the Round Top Festival Institute and the New York Symphonic Ensemble's Tour of Japan. 
+</p>
+<p>
+Matthew holds a Master of Music degree from Temple University. Matthew&#39;s musical journey has been enriched immensely by extensive study under the guidance of Peter Smith, whose mentorship has proven to be invaluable in shaping his artistic path. A native of Iowa, Matthew studied with renowned pedagogue Andrew Parker at the University of Iowa, laying the foundation for his musical endeavors in Philadelphia. Matthew would also like to acknowledge others who have helped, guided, or inspired him along the way, including Erin Hannigan, Nicholas Stovall, Philippe Tondre, Jonathan Blumenfeld, Elizabeth Starr Masoudnia, and John Symer.
+</p>
+        {/* <p>
           Matthew Shipp is one of the East Coast&#39;s most sought-after freelance oboists.
           They regularly perform with some of the region&#39;s most distinguished ensembles, including 
            The Philadelphia Orchestra, Reading Symphony Orchestra, Pennsylvania Ballet, Chamber
@@ -32,7 +45,7 @@ export default function Bio() {
           summers participating in music festivals such as the Round Top
           Festival Institute and the New York Symphonic Ensemble&#39;s Tour of
           Japan.
-        </p>
+        </p> */}
       </article>
     </div>
   );
